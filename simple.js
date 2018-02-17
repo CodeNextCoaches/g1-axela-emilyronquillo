6950d2011152cf7b7be1f0afc6a9eda578585a48
Student <student@Admins-MacBook-Air.local>
2018-02-17 13:41:34 -0800

    first commit

--- simple.js
+++ simple.js
@@ -1,6 +1,8 @@
 (function () {
 
-  var input,
+  var input = document.querySelector('#iris-input'),
+      submit = document.querySelector('#iris-submit'),
+      message = document.querySelector('#iris-message'),
       submit,
       message,
       validInputs = [
@@ -15,9 +17,11 @@
         "I am old enough. That's all you need to know.",
         "42. Duh.",
         "What do you call a swindler going down some stairs? <br>Condescending. Ha!"];
+        console.log(input);
 
   // Add event listeners to text input and submit button below
-
+  submit.addEventListener('click', processInput);
+  input.addEventListener('keyPress', checkKey);
 
   // This function checks if the user has pressed "ENTER" on their keyboard.
   function checkKey(event) {
@@ -41,5 +45,11 @@
    *  validInputs array), the response would be "42. Duh" (index 3 in the
    *  responses array).
    */
+
+   function processInput(){
+     let currentInput = input.value;
+     input.value = '';
+     alert(currentInput);
+   }
 
 })();
